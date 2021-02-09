c5baede97e9bcb9dce206c73b18beeeac8397bc5
ACG Parnell College <57234021+ACGparnell@users.noreply.github.com>
2021-02-10 07:54:32 +1300

    Update __init__.py

--- factorial/__init__.py
+++ factorial/__init__.py
@@ -16,6 +16,11 @@ def TestNine():
     check50.run("python3 factorial.py").stdin("9").stdout("362880").exit(0)
     
 @check50.check(exists)
+def Test0099():
+    """responds to 99"""
+    check50.run("python3 factorial.py").stdin("99").stdout("933262154439441526816992388562667004907159682643816214685929638952175999932299156089414639761565182862536979208272237582511852109168640000000000000000000000").exit(0)
+    
+@check50.check(exists)
 def testNegative():
     """rejects a negative"""
     check50.run("python3 factorial.py").stdin("-5").reject()
